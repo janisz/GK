94a89a6a8defe31002f5f8ce540c8ebf7b814d44
Tomek Janiszewski <janiszt@gmail.com>
2012-10-17 10:49:49 +0200

    Fix AACircle drawing direction

--- paintarea.cpp
+++ paintarea.cpp
@@ -94,7 +94,7 @@ void PaintArea::mouseMoveEvent(QMouseEvent *event)
             s = Canvas.Circle(QPoint(startPoint.x()+r*sgn(e.x()), startPoint.y()+r*sgn(e.y())), std::abs(r), lineColor);
         break;
         case AACircle:
-            s = (Canvas.AACircle(QPoint(startPoint.x()+r, startPoint.y()+r), std::abs(r), lineColor));
+        s = (Canvas.AACircle(QPoint(startPoint.x()+r*sgn(e.x()), startPoint.y()+r*sgn(e.y())), std::abs(r), lineColor));
         break;
         case Ellipse:
             s = Canvas.Ellipse(startPoint, event->pos(), lineColor);
